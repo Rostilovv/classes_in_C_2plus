5e244292c9f99f653ae2ecb8bb767f6b76c85e5f
Rostilovv <88591549+Rostilovv@users.noreply.github.com>
2022-06-06 14:43:27 +0300

    Create The_first_digit_of_a_two-digit_number.cpp

---
+++ integers/The_first_digit_of_a_two-digit_number.cpp
@@ -0,0 +1,10 @@
+//Дано двузначное число. Выведите его первую цифру (число десятков)
+
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+  cin >> x;
+  cout << x / 10;
+  return 0;
+}
